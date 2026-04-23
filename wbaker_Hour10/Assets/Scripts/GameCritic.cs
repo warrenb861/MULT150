098b92ba71595bea623b6d58e0c3be683ff36d12
warrenb861 <warrenb861@gmail.com>
2026-04-23 09:04:27 -0700

    Hour10

---
+++ wbaker_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,11 @@
+/*
+Improving The Game Questions:
+
+1. The game feels too easy. If you just stand still it feels like most of the time the balls will just go into the goals by themselves with no input needed.
+
+2. What might make the game harder is if the balls eventually lost momentum and slowed down so you would be forced to interact with them to make them bounce again.
+
+3.
+
+4. It is fun watching the balls bounce around, but moving around feels a little bit sluggish.
+*/
---
+++ wbaker_Hour10/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public bool isSolved = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject collidedWith = collider.gameObject;
+        if (collidedWith.tag == gameObject.tag)
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+        }
+    }
+}
