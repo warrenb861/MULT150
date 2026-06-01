1224b1f8dcedb81b91c56afd7747c31f8ffc7ea8
warrenb861 <warrenb861@gmail.com>
2026-05-31 18:55:02 -0700

    Final

---
+++ wbaker_Final/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "wbaker_Final";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
---
+++ wbaker_Final/Assets/Modifications.cs
@@ -0,0 +1,58 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: Changed x-rotation of light, increased intensity of light, and gave the light a faint light-blue color
+
+        2 - fog
+        modifications: Added subtle amount of fog with a faint blue hue to it.
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications:  Created light-blue sky box material called "MySkybox" and applied it to the environment.
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: Added light to obstacle prefab and increased intensity and gave it a subtle yellow hue.
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications:  made a new powerup prefab that looks like a face
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: FOR EYE: https://www.pngegg.com/en/png-fmcoh
+        texture source 2: FOR MOUTH (cropped afterwards): https://www.shutterstock.com/image-photo/isolated-lip-closeup-macro-beautiful-mouth-1075292240?dd_referrer=https%3A%2F%2Fwww.google.com%2F
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: changed color to orange
+        parameter 2: changed shape to donut
+        parameter 3: changed start speed to -0.05
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
